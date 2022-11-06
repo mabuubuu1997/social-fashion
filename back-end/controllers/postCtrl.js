1af02664f21fc5e6ec40f96271d8f41ac9376f2e
Phạm Long <dantxgcd18323@fpt.edu.vn>
2022-11-06 17:23:50 +0700

    update post && delete post with auth

--- back-end/controllers/postCtrl.js
+++ back-end/controllers/postCtrl.js
@@ -1,6 +1,5 @@
 const PostModel = require('../models/postModel')
 
-
 const post_controller = {
 
     createPost: async(req, res) =>{
@@ -35,6 +34,43 @@ const post_controller = {
             res.status(200).json(postMessages);
         } catch (err) {
             res.status(404).json({msg: err.message});
+        }
+    },
+    updatePost: async(req, res) =>{
+        try
+        {
+            const ObjectId = require('mongoose').Types.ObjectId
+            const { id } = req.params;
+            const { title, message, creator, images, tags } = req.body;
+        
+            if (!ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    
+            const updatedPost = { creator, title, message, tags, images, _id: id };
+    
+            await PostModel.findByIdAndUpdate(id, updatedPost)
+            .populate("user likes", "avatar name")
+            .populate({
+                path: "comment",
+                populate: {
+                    path: "user likes",
+                    select: "comment"
+                }
+            });
+    
+            res.json({
+                updatedPost: {...updatedPost._doc,
+                title, message, tags, images}});
+        } catch(err){
+            res.status(400).json({msg: err.message});
+        }
+    },
+
+    deletePost: async(req, res) =>{
+        try {
+            await PostModel.findOneAndDelete({_id: req.params.id, post:req.user._id});
+            res.json({ message: "Post deleted successfully." });
+        } catch (err) {
+            res.status(400).json({msg: err.message});
         }
     },
 
--- back-end/routes/postRouter.js
+++ back-end/routes/postRouter.js
@@ -3,7 +3,8 @@ const postCtrl = require("../controllers/postCtrl");
 const user = require("../middleware/user")
 
 router.get('/getPosts', user, postCtrl.getPosts)
-
 router.post('/createPost', user, postCtrl.createPost)
+router.patch('/updatePost/:id',user,  postCtrl.updatePost)
+router.delete('/deletePost/:id',user, postCtrl.deletePost)
 
 module.exports = router;
